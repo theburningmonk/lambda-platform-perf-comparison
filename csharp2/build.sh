f08cf9ee48695a12db4ae0984cef1951277dcb61
Yun Zhi Lin <yun@yunspace.com>
2018-01-23 01:20:07 +1100

    simplified csproj and fsproj together with build arguments

--- csharp2/build.sh
+++ csharp2/build.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-#build handlers
-dotnet restore
-dotnet publish -c release
-
-#install zip
-# apt-get -qq update
-# apt-get -qq -y install zip
-
-#create deployment package
-pushd bin/release/netcoreapp2.0/publish
-zip -r ./deploy-package.zip ./*
-popd
+dotnet lambda package --configuration release --framework netcoreapp2.0 --output-package bin/release/netcoreapp2.0/publish/deploy-package.zip
