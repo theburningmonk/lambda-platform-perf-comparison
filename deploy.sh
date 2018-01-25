fae1d48ae4a846336b87da66c44ef7e8fa33637a
Yunspace <yun@yunspace.com>
2018-01-26 00:30:36 +1100

    make deploy region configurable

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@
 declare -a folders=("csharp" "csharp2" "fsharp" "fsharp2" "go" "java" "python" "python3" "nodejs4" "nodejs6")
 
 export AWS_PROFILE=personal
+export AWS_REGION=us-east-1
 
 for i in `seq 1 10`;
 do
@@ -10,7 +11,7 @@ do
     cd $folder
     pwd
     
-    sls deploy --region ap-southeast-2
+    sls deploy --region $AWS_REGION
 
     cd ..
   done
