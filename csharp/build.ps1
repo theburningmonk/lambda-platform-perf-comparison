7bce21c9c96c4c19dde118a5593e8bc8f498900a
Yunspace <yun@yunspace.com>
2018-01-25 17:45:16 +1100

    added restore to all dotnet build scripts

--- csharp/build.ps1
+++ csharp/build.ps1
@@ -1,1 +1,2 @@
+dotnet restore
 dotnet lambda package --configuration release --framework netcoreapp1.0 --output-package bin/release/netcoreapp1.0/publish/deploy-package.zip
--- csharp/build.sh
+++ csharp/build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+dotnet restore
 dotnet lambda package --configuration release --framework netcoreapp1.0 --output-package bin/release/netcoreapp1.0/publish/deploy-package.zip
