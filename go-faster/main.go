8003b580627f3665577e737eac59eb3a845443b2
Yunspace <yun@yunspace.com>
2018-04-07 21:27:38 +1000

    deployed working version

--- go-faster/main.go
+++ go-faster/main.go
@@ -1,16 +1,14 @@
 package main
 
 import (
-	"context"
-
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/yunspace/aws-lambda-go-faster/lambda"
 )
 
-func Handler(ctx context.Context, request *events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+func Handler(request *events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	return &events.APIGatewayProxyResponse{Body: "hello", StatusCode: 200}, nil
 }
 
 func main() {
-	lambda.Start(Handler, events.APIGatewayProxyRequest)
+	lambda.Start(Handler)
 }
--- go/main.go
+++ go/main.go
@@ -1,13 +1,11 @@
 package main
 
 import (
-	"context"
-
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func Handler(ctx context.Context, request *events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
+func Handler(request *events.APIGatewayProxyRequest) (*events.APIGatewayProxyResponse, error) {
 	return &events.APIGatewayProxyResponse{Body: "hello", StatusCode: 200}, nil
 }
 
